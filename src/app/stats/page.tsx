9b4316a53507e2fbde09d57460239bbb719c5ebe
Mario <mariovidovic97@gmail.com>
2025-07-30 15:18:22 +0200

    Find link for stats page created

---
+++ src/app/stats/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import styles from "./SearchLink.module.scss";
+
+export default function SearchLinkPage() {
+  return (
+    <section className={styles.SearchLinkPage}>
+      <div className="wrapper"></div>
+    </section>
+  );
+}
