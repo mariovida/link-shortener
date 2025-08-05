054fa5cbcd645853000a20c7193ed67657cbf827
Mario <mariovidovic97@gmail.com>
2025-08-05 16:41:02 +0200

    Bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import UrlShortener from "../components/UrlShortener";
-import "../styles/globals.scss";
 
 export default function Page() {
-  return (
-    <main>
-      <UrlShortener />
-    </main>
-  );
+  return <UrlShortener />;
 }
